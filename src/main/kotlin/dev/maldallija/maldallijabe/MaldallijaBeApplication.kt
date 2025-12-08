b9726ccb116b50cec3d3e2e1327a9f3ef7494da2
49EHyeon42 <49ehyeon42@gmail.com>
2025-12-08 22:26:27 +0900

    init: 프로젝트 시작

---
+++ src/main/kotlin/dev/maldallija/maldallijabe/MaldallijaBeApplication.kt
@@ -0,0 +1,11 @@
+package dev.maldallija.maldallijabe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MaldallijaBeApplication
+
+fun main(args: Array<String>) {
+    runApplication<MaldallijaBeApplication>(*args)
+}
---
+++ src/test/kotlin/dev/maldallija/maldallijabe/MaldallijaBeApplicationTests.kt
@@ -0,0 +1,12 @@
+package dev.maldallija.maldallijabe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MaldallijaBeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}

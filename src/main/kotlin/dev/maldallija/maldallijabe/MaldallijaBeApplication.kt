2c6972fa20ea297e5d06c47c82ef48c1eb90ceff
49EHyeon42 <49ehyeon42@gmail.com>
2025-12-09 20:42:52 +0900

    chore(auth): 스프링 시큐리티 비활성화

--- src/main/kotlin/dev/maldallija/maldallijabe/MaldallijaBeApplication.kt
+++ src/main/kotlin/dev/maldallija/maldallijabe/MaldallijaBeApplication.kt
@@ -1,9 +1,16 @@
 package dev.maldallija.maldallijabe
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = [
+        SecurityAutoConfiguration::class,
+        ManagementWebSecurityAutoConfiguration::class,
+    ],
+)
 class MaldallijaBeApplication
 
 fun main(args: Array<String>) {

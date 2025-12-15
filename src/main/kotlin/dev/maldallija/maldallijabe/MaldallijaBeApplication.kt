ad1367d4388daa4885dcc3bf7747fd239f8e6c98
49EHyeon42 <49ehyeon42@gmail.com>
2025-12-15 22:24:29 +0900

    fix(auth): 스프링 시큐리티 기본 UserDetailsService를 사용하지 않도록 설정

--- src/main/kotlin/dev/maldallija/maldallijabe/MaldallijaBeApplication.kt
+++ src/main/kotlin/dev/maldallija/maldallijabe/MaldallijaBeApplication.kt
@@ -2,14 +2,14 @@ package dev.maldallija.maldallijabe
 
 import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication(
     exclude = [
-        SecurityAutoConfiguration::class,
         ManagementWebSecurityAutoConfiguration::class,
+        UserDetailsServiceAutoConfiguration::class,
     ],
 )
 @ConfigurationPropertiesScan

12a1e937142cbb76b2c21ad5808ebaa62071e0c3
49EHyeon42 <49ehyeon42@gmail.com>
2025-12-21 10:54:09 +0900

    fix(global): 빈 생성 제외 로직을 application.yaml로 이동

--- src/main/kotlin/dev/maldallija/maldallijabe/MaldallijaBeApplication.kt
+++ src/main/kotlin/dev/maldallija/maldallijabe/MaldallijaBeApplication.kt
@@ -1,17 +1,10 @@
 package dev.maldallija.maldallijabe
 
-import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-    exclude = [
-        ManagementWebSecurityAutoConfiguration::class,
-        UserDetailsServiceAutoConfiguration::class,
-    ],
-)
+@SpringBootApplication
 @ConfigurationPropertiesScan
 class MaldallijaBeApplication
 
